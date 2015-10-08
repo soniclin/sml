90a976caa36b83d04ae46465d61e01872b8924bf
soniclin <sonic.660357@gmail.com>
2015-10-09 03:25:22 +0800

    add specialized template

--- Math.cpp
+++ Math.cpp
@@ -4,10 +4,13 @@
 namespace sml
 {
 
+	template<>
 	double Math<double>::pi_value = static_cast<double>(4. * std::atan(1.));
 
+	template<>
 	bool Math<double>::eps_state = false;
 
+	template<>
 	double Math<double>::epsilon =  Math<double>::reset_eps();
 
 }
